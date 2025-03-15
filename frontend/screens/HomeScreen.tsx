3e8af64b8cde431fa5768f5f215e2830a87d71d7
Adam D <adamd@Adams-MacBook-Air-2.local>
2025-03-15 03:31:25 -0500

    added basic ui

--- frontend/screens/HomeScreen.tsx
+++ frontend/screens/HomeScreen.tsx
@@ -1,22 +1,48 @@
 import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, FlatList, Dimensions } from 'react-native'
+import ClassEntry from "../components/ClassEntry"
+
 
 export default function HomeScreen() {
+  interface Item {
+    id: string;
+    className: string;
+    members: number;
+    icon: string;
+  }
+
+  const data = [
+    { id: '1', className: 'CSCI 1133', members: 27, icon: "" },
+    { id: '2', className: 'CSCI 1933', members: 30, icon: ""},
+    { id: '3', className: 'CSCI 2021', members: 33, icon: "" },
+  ];
+  const renderItem = ({ item }: { item: Item }) => (
+    <ClassEntry className={item.className} members={item.members} icon={item.icon} />
+  );
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Welcome to the Home Screen</Text>
-    </View>
+    <FlatList
+        data={data}
+        renderItem={renderItem}
+        keyExtractor={(item) => item.id}
+        numColumns={2}
+        columnWrapperStyle={styles.row}
+        contentContainerStyle={styles.container}
+    />
   )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    paddingTop: 10,
   },
-  text: {
-    fontSize: 18,
-    fontWeight: 'bold',
+  flatList: {
+    flexGrow: 1,
+    paddingBottom: 20,
   },
-})
+  row: {
+    flex: 1,
+    justifyContent: 'space-evenly',
+  },
+});
+
